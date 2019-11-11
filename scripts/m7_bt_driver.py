4c0e2ca2b127d34da9ef0f9df707e0ff61599322
hcyoo93 <47550643+hcyoo93@users.noreply.github.com>
2019-11-12 01:48:15 +0900

    Add files via upload

--- scripts/m7_bt_driver.py
+++ scripts/m7_bt_driver.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 
-'''
-File name:robot_bt_driver.py
-
-This is ROS driver node for mobile robot using bluetooth, it is using python bluetooth module for sending and receiving data.
-
-This will publish robot IMU, speed and encoder data, also send speed and reset command to robot
-
-Author: Lentin Joseph
-
-'''
-
-
 #ROS Modules
 import rospy
 #from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float32
 from std_msgs.msg import Int32, Int64
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Int32MultiArray
 
 import string
 import time
@@ -25,49 +13,47 @@ import bluetooth
 
 import signal
 import sys
+import math
 
 ##########################################################################################
+
 #Bluetooth parameters
 
-#Robot's Bluetooth address
-bluetooth_mac = '20:16:04:18:61:60'
-
-
+#BL Robot's Bluetooth address
+bluetooth_mac = '98:D3:31:FD:3E:0D'
 
 rospy.init_node('ros_bluetooth_driver',anonymous=True)
-#orient = Vector3()
 
 rospy.loginfo("Staring ROS-Bluetooth Driver node")
 
 ##########################################################################################
-#Speed handler, this will send speed to BL
 
-#def speed_send(data):
+#Speed handler, this will send speed to BL 
+#Send control input to arduino after receive message from subscriber '/set_control'
+
 def control_send(data):
 
     	global bluetooth_serial_handle	
 
 	#Note, here we may need change in one motor speed, now taking both speed as data
 	try:
-
-                #left_speed = data.data[0]
-                #right_speed = data.data[1]
-                steer = data.data[0]
-                speed = data.data[1]
-
+                steer_val = data.data[0] # assuming 2digits:ab
+                speed_val = data.data[1] # assuming 3digits:cde
+	        
+		
+			a=math.floor(steer_val/10)
+			b=steer_val-10*a
+		
+			c=math.floor(speed_val/100)
+			d=math.floor(speed_val/10)-10*c		
+			e=speed_val-100*c-10*d
+		
 	except:
 		rospy.logwarn("Found exception in BT driver node")
                 steer = 0.0
                 speed = 0.0
-		
-	#print data.data[0]
-	#rospy.loginfo( data.data[1]
-
-	#print 'Setting left speed = %d : right speed=%d' %(left_speed,right_speed)
-
-		
-        send_data = 's %f %f\r' %(float(steer),float(speed))
-
+	
+	send_data = '%d%d%d%d%d' %(int(a),int(b),int(c),int(d),int(e))
 
 	try:
 		bluetooth_serial_handle.send(str(send_data))
@@ -75,18 +61,8 @@ def control_send(data):
 		rospy.logwarn("Unable to send BL data")
 		pass
 	
-	#Command to give as command line 
-	'''
-	rostopic pub /set_speed std_msgs/Int32MultiArray "layout:
-	  dim:
-	  - label: ''
-	    size: 0
-	    stride: 0
-	  data_offset: 0
-	data: [50,50]"
-	'''
 ##########################################################################################
-def reset_robot(data):
+'''def reset_robot(data):
     	global bluetooth_serial_handle	
 
 	send_data = 'r\r'
@@ -98,40 +74,21 @@ def reset_robot(data):
 		rospy.sleep(1)
 	except:
 		rospy.logwarn("Unable to send BL data")
-		pass
-
+		pass '''
 
 ##########################################################################################
 
-#Publishers
+#Publishers (publish sensor value to main computer)
+ 
+#comm_check_handle = rospy.Publisher('comm_check', Int32, queue_size=1)
+ 
+#Subscribers (receive control input from main computer(ROS))
 
- #left_speed_handle = rospy.Publisher('left_speed', Int32, queue_size=1)
- #right_speed_handle = rospy.Publisher('right_speed', Int32, queue_size=1)
-
-comm_check_handle = rospy.Publisher('comm_check', Int32, queue_size=1)
-
- #left_encoder_handle = rospy.Publisher('left_ticks', Int32, queue_size=1)
- #right_encoder_handle = rospy.Publisher('right_ticks', Int32, queue_size=1)
-
- #imu_yaw_handle = rospy.Publisher('yaw', Float32, queue_size=1)
- #imu_pitch_handle = rospy.Publisher('pitch', Float32, queue_size=1)
- #imu_roll_handle = rospy.Publisher('roll', Float32, queue_size=1)
-
- #imu_data_handle = rospy.Publisher('imu_data', Vector3, queue_size=1)
-
- #ultrasonic_handle = rospy.Publisher('obstacle_distance', Int64, queue_size=1)
-
-#Subscribers
-
-rospy.Subscriber('/set_control', Float32MultiArray, control_send)
+rospy.Subscriber('/set_control', Int32MultiArray, control_send)
 rospy.Subscriber('/reset', Int32, reset_robot)
 
-
-
-
 ##########################################################################################
-
-#Key board handler
+'''#Key board handler
 
 def quit_code(signum, frame):
 
@@ -141,32 +98,16 @@ def quit_code(signum, frame):
 	rospy.loginfo("Quitting code")
 	sys.exit(1)
 
-signal.signal(signal.SIGINT, quit_code)
+signal.signal(signal.SIGINT, quit_code)'''
 
 ##########################################################################################
 
-
-
-
 #Function to publish topics to ROS
-def publish_topics(string_list):
-        #global orient
+
+'''def publish_topics(string_list):
+        
          global comm_check_handle
-        #global left_speed_handle
-        #global right_speed_handle
-
-        #global left_encoder_handle
-        #global right_encoder_handle
-
-
-        #global imu_yaw_handle
-        #global imu_pitch_handle
-        #global imu_roll_handle
-
-        #global imu_data_handle
-
-        #global ultrasonic_handle
-	
+        
 
 	#Reading each line in list and spliting it
 	for line in string_list:
@@ -179,55 +120,26 @@ def publish_topics(string_list):
                         #right_speed_handle.publish(float(real_data[2]))
 
                 if(real_data[0] == 's'):
-                        comm_check_handle.publish(float(real_data[1]))
+                        comm_check_handle.publish(float(real_data[1]))'''
 
-
-                #if(real_data[0] == 'e'):
-                        #left_encoder_handle.publish(float(real_data[1]))
-                        #right_encoder_handle.publish(float(real_data[2]))
-
-                #if(real_data[0] == 'i'):
-                        #imu_yaw_handle.publish(float(real_data[1]))
-                        #imu_pitch_handle.publish(float(real_data[2]))
-                        #imu_roll_handle.publish(float(real_data[3]))
-
-
-                        #orient.x = float(real_data[1])
-                        #orient.y = float(real_data[2])
-                        #orient.z = float(real_data[3])
-
-                        #imu_data_handle.publish(orient)
-
-                #if(real_data[0] == 'u'):
-                        #try:
-                                #ultrasonic_handle.publish(int(real_data[1]))
-                        #except:
-                                #pass
 ##########################################################################################
-#Function to remove spaces from sentence 
 
+#Function to remove spaces from decoded sentence and publish sensor value to ROS(main computer)
 
-
-def remove_space(sentence):
+'''def remove_space(sentence):
 	org_sentence = []
 	for char in sentence:
 		if(char != ""):
 			org_sentence.append(char)
 
 	#print org_sentence
-	publish_topics(org_sentence)
+	publish_topics(org_sentence)'''  
 	
 ##########################################################################################
-
-'''def decode_string(string):
-        This function will decode the buffer data and
-	convert into each string which contain ypr values
-	param: string, string buffer
-
-
-	start_flag = 0
-
-	final_string = ""
+#functions for decode sensor value from robot(sent from arduino)
+'''def decode_string(string): 
+        #This function will decode the buffer data 
+	#param: string, string buffer
 
 	string_list = []
 
@@ -253,82 +165,11 @@ def remove_space(sentence):
 
 			final_string = ''
 
-	final_string = remove_space(string_list)
-
-
-	string_list = []
-	final_string  = ""
-        #Encoder value extract
-	for i in range(0,string_len):
-		if(decode_list[i] == 'e' and (string_len - i) > 6):
-			start_flag = 1
-
-		if(start_flag == 1):
-			if(decode_list[i] != '\n'):
-				final_string = final_string + decode_list[i]
-			else:
-				start_flag = 0
-		#If it is a new line, append to a list
-		if(decode_list[i] == "\n"):
-			string_list.append(final_string)
-
-			final_string = ''
-	
-	final_string = remove_space(string_list)
-
-
-
-	string_list = []
-	final_string  = ""
-
-        #Motor speed value extract
-	for i in range(0,string_len):
-		#Start reading when y is found
-		if(decode_list[i] == 's' and (string_len - i) > 8):
-			start_flag = 1
-
-		if(start_flag == 1):
-			if(decode_list[i] != '\n'):
-				final_string = final_string + decode_list[i]
-			else:
-				start_flag = 0
-		#If it is a new line, append to a list
-		if(decode_list[i] == "\n"):
-			string_list.append(final_string)
-
-			final_string = ''	
-
-
-
-
-
-	string_list = []
-	final_string  = ""
-
-        #Motor speed value extract
-	for i in range(0,string_len):
-		#Start reading when y is found
-		if(decode_list[i] == 'u' and (string_len - i) > 4):
-			start_flag = 1
-
-		if(start_flag == 1):
-			if(decode_list[i] != '\n'):
-				final_string = final_string + decode_list[i]
-                        else:
-                                start_flag = 0
-		#If it is a new line, append to a list
-                if(decode_list[i] == "\n"):
-                        string_list.append(final_string)
-
-                        final_string = ''
-
-
-
-
-        final_string = remove_space(string_list)'''
+	final_string = remove_space(string_list)'''
 
 ##########################################################################################
-#Function to connect to BL robot
+
+#Function to connect to BL robot: establish bluetooth connection 
 	
 def connect():
     global bluetooth_mac
@@ -347,21 +188,20 @@ def connect():
 
 bluetooth_serial_handle = connect()
 
-
 ##########################################################################################
 
-#Main code
+#Main code : receive data from arduino 
 
 if __name__ == '__main__':
 
 	while(True):
 	    try:
-		
 		receive_data = bluetooth_serial_handle.recv(300)
-		list_rec = receive_data.split("\n")
-		list_rec = list(receive_data)
+		#list_rec = receive_data.split("\n")
+		#list_rec = list(receive_data)
 		#Continously receiving data from robot and decode it
-		decode_string(list_rec)
+		#decode_string(receive_data)
+		rospy.loginfo(str(receive_data))
 
 		rospy.sleep(0.05)
 
