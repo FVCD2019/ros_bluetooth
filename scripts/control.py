b44614bc4174e76b59b99c5e56e7ca3bbc5afb26
beom <qjadud1994@gmail.com>
2019-11-22 16:55:05 +0900

    update

--- scripts/control.py
+++ scripts/control.py
@@ -36,7 +36,7 @@ def signal_handler(signal,frame):
 signal.signal(signal.SIGINT,signal_handler)
 
 def control_send(data):
-    global bluetooth_serial_handle
+	global bluetooth_serial_handle
 
 	#Note, here we may need change in one motor speed, now taking both speed as data
 	try:
@@ -63,8 +63,8 @@ def control_send(data):
 
 	except:
 		rospy.logwarn("Found exception in BT driver node")
-                steer_val = 0
-                speed_val = 0
+		steer_val = 0
+		speed_val = 0
 
 	send_data = '%d%d%d%d%d' %(int(a),int(b),int(c),int(d),int(e))
 	print(send_data)
@@ -83,12 +83,12 @@ rospy.Subscriber('/cmd_vel', Twist, control_send)
 
 ##########################################################################################
 def stop():
-    global bluetooth_serial_handle
+	global bluetooth_serial_handle
 
 	#Note, here we may need change in one motor speed, now taking both speed as data
 
-    steer_val = 0
-    speed_val = 0
+	steer_val = 0
+	speed_val = 0
 	a = math.floor(steer_val/10)
 	b = steer_val-10*a
 	c = math.floor(speed_val/100)
@@ -108,19 +108,19 @@ def stop():
 #Function to connect to BL robot: establish bluetooth connection
 
 def connect():
-    global bluetooth_mac
-    global bluetooth_serial_handle
-    while(True):
-        try:
-            bluetooth_serial_handle = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-            bluetooth_serial_handle.connect((bluetooth_mac, 1))
-            break;
-        except bluetooth.btcommon.BluetoothError as error:
-            bluetooth_serial_handle.close()
-	    rospy.logwarn("Unable to connect, Retrying in 10s...")
-            #print "Could not connect: ", error, "; Retrying in 10s..."
-            time.sleep(10)
-    return bluetooth_serial_handle;
+	global bluetooth_mac
+	global bluetooth_serial_handle
+	while(True):
+		try:
+			bluetooth_serial_handle = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+			bluetooth_serial_handle.connect((bluetooth_mac, 1))
+			break;
+		except bluetooth.btcommon.BluetoothError as error:
+			bluetooth_serial_handle.close()
+			rospy.logwarn("Unable to connect, Retrying in 10s...")
+			#print "Could not connect: ", error, "; Retrying in 10s..."
+			time.sleep(10)
+	return bluetooth_serial_handle;
 
 bluetooth_serial_handle = connect()
 
@@ -131,17 +131,16 @@ bluetooth_serial_handle = connect()
 if __name__ == '__main__':
 
 	while(True):
-	    try:
+		try:
+			receive_data = bluetooth_serial_handle.recv(300)
+			rospy.loginfo(str(receive_data))
+			rospy.sleep(0.05)
 
-		receive_data = bluetooth_serial_handle.recv(300)
-		rospy.loginfo(str(receive_data))
-		rospy.sleep(0.05)
-
-	    except bluetooth.btcommon.BluetoothError as error:
-		#print "Caught BluetoothError: ", error
-		rospy.logerr("Caught Bluetooth Error; error")
-		time.sleep(5)
-		bluetooth_serial_handle = connect()
-		pass
+		except bluetooth.btcommon.BluetoothError as error:
+			#print "Caught BluetoothError: ", error
+			rospy.logerr("Caught Bluetooth Error; error")
+			time.sleep(5)
+			bluetooth_serial_handle = connect()
+			pass
 
 	#bluetooth_serial_handle.close()
